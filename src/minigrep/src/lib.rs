d3f69ef964761a75b366c6f584ad857dc9eeeefe
hellowac <1165121284@qq.com>
2022-12-29 21:44:39 +0800

    将minigrep的search应用到run中

--- src/minigrep/src/lib.rs
+++ src/minigrep/src/lib.rs
@@ -22,13 +22,25 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("文件内容: \n{}", contents);
+    // println!("文件内容: \n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -40,9 +52,9 @@ mod tests {
         let query = "duct";
         // 这告诉 Rust 不要在字符串字面值内容的开头加入换行符
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
